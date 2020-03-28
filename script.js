f3fa0dd7b20a2db1e63eb0552cd100a215fd35c3
Jack de la Motte <jack.delamotte13@gmail.com>
2020-03-27 19:13:33 -0700

    made a bunch of changes
    quit()
    ''

---
+++ script.js
@@ -0,0 +1,11 @@
+function visitHomePage(){
+    window.location = "index.html";
+};
+
+function visitAboutPage(){
+    window.location = "about.html"
+};
+
+function visitSuggestionsPage(){
+    window.location = "suggestions.html"
+};
